223415346ab33a2b85409d2e57e98e4a777adf6b
Álysson Gleyson <alyssongleysondasilva@gmail.com>
2025-04-25 19:52:16 -0300

    feat: add directory configuration variables

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+###################################################################
+# Script Name:	backup.sh
+# Description:	Script for backup automation
+# Author:	Álysson Gleyson da Silva
+###################################################################
+
+# Directories
+source_dir="$HOME/Documents"
+backup_dir="$HOME/backup"
+log_file="$HOME/backup_log.txt"
+
+
