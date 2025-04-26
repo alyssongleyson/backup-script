329461293fba44135a6b56a3c8deec4848721413
Álysson Gleyson <alyssongleysondasilva@gmail.com>
2025-04-26 11:33:13 -0300

    feat: checking for existence of backup directory

--- backup.sh
+++ backup.sh
@@ -18,4 +18,11 @@ backup_dir_with_date="$backup_dir/backup_$current_date"
 # Create backup directory
 mkdir -p "$backup_dir_with_date"
 
-
+# Check if the 'latest' directory exists
+if [ -d "$backup_dir/latest" ]; then
+    # Performs incremental backup (rsync) with link to the most recent directory
+    rsync -av --link-dest="$backup_dir/latest" "$source_dir" "$backup_dir_with_date"
+else
+    # If the 'latest' directory does not exist, just copy the files
+    rsync -av "$source_dir" "$backup_dir_with_date"
+fi
